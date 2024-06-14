0345d74af001c83fde7d1f557c3f10937259daff
Gael Adam <gaelswim@hotmail.com>
2024-06-14 13:04:58 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+    <title>Arcadia</title>
+</head>
+    <header>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+    </header>
+    <body>
+    <?php
+    //require "./Animal.php";
+    require "./header.php";
+    ?>
+<main>
+    <h3 class="text-center">Bienvenue au Zoo Arcadia</h3>
+</main>
+</body>
+
+<footer>
+    <?php
+    require "./footer.php";
+    ?>
+</footer>
+</html>
