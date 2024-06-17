9c0eefb66373177c39971a8bdd0714142e90b700
Gael <gaelswim@hotmail.com>
2024-06-17 17:06:29 +0200

    index

--- index.php
+++ index.php
@@ -1,13 +1,16 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Arcadia</title>
 </head>
+    <body>
     <header>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
     </header>
-    <body>
     <?php
     //require "./Animal.php";
     require "./header.php";
@@ -15,11 +18,13 @@
 <main>
     <h3 class="text-center">Bienvenue au Zoo Arcadia</h3>
 </main>
-</body>
-
 <footer>
     <?php
     require "./footer.php";
     ?>
 </footer>
+    
+</body>
+
+
 </html>
